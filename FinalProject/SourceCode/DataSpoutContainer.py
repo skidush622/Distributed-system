4392d57b19589ff99d17c6f843516d9dde266649
ZhuangweiKang <zkty9@mail.missouri.edu>
2018-04-23 22:06:15 -0500

    update

--- FinalProject/SourceCode/DataSpoutContainer.py
+++
@@ -1,4 +0,0 @@
-import docker
-
-client = docker.from_env()
-client.images.build(path='./DockerFiles/DataSpoutDockerFile', tag='DataSpout')
---
+++ FinalProject/SourceCode/DataSpoutContainers.py
@@ -0,0 +1,47 @@
+import docker
+import argparse
+
+
+class DataSpoutContainers:
+    def __init__(self):
+        self.client = docker.from_env()
+        self.dockerfile_path = './DockerFiles/DataSpoutDockerFile'
+        self.image_tag = 'dataspout'
+        self.image = None
+
+    def build_image(self):
+        self.image = self.client.images.build(path='./DockerFiles/DataSpoutDockerFile', tag=self.image_tag)
+
+    def run_container(self, name, hostname, command):
+        container = self.client.containers.run(image=self.image,
+                                               detach=True,
+                                               name=name,
+                                               hostname=hostname,
+                                               command=command)
+        return container
+
+    def main(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-a', '--address', type=str, default='127.0.0.1', help='The address of destination.')
+        parser.add_argument('-p', '--port', type=str, default='2341', help='The port number of destination.')
+        args = parser.parse_args()
+        address = args.address
+        port = args.port
+        for i in range(5):
+            container_name = 'DataSpout' + str(i+1)
+            command = 'python SpoutingData.py -s ' + str(i+1) + ' -a ' + address + ' -p ' + port
+            self.run_container(container_name, container_name, command)
+
+
+if __name__ == '__main__':
+    spout = DataSpoutContainers()
+    spout.build_image()
+    spout.main()
+
+
+
+
+
+
+
+
