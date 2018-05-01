dbe3b76ecf81b1457d9acf7b9654d8d872dda0cc
ZhuangweiKang <zkty9@mail.missouri.edu>
2018-05-01 17:28:41 -0500

    update

--- FinalProject/SourceCode/K8sPart/Egress.py
+++ FinalProject/SourceCode/K8sPart/Egress.py
@@ -150,7 +150,7 @@ class Egress:
 				self.flag = 0
 				self.lock.acquire()
 				# 读取前5/row_count 行数据
-				data = mysqlop.query_first_N(self.db_handler, self.db_name, self.tb_name, 5)
+				data = mysqlop.query_first_N(self.db_handler, self.db_name, self.tb_name, 2)
 				temp_data = []
 				for item in data:
 					temp_data.append({'ID': item[0], 'State': item[1], 'Sum': item[3], 'Mean': item[4], 'Max': item[5], 'Min': item[6]})
