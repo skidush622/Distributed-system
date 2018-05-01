632ca20552cae59383bba4042668b1283de00521
ZhuangweiKang <zkty9@mail.missouri.edu>
2018-05-01 17:35:02 -0500

    update

--- FinalProject/SourceCode/K8sPart/Egress.py
+++ FinalProject/SourceCode/K8sPart/Egress.py
@@ -121,6 +121,7 @@ class Egress:
 		while True:
 			data = self.up_stream_socket.recv_string()
 			data = simplejson.loads(data)
+			print(data)
 			id = data['ID']
 			self.up_stream_socket.send_string('Ack--' + str(id))
 			state = data['State']
